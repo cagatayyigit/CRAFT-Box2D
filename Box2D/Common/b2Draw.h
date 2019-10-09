03d07747c5dca6c03e63a5a26cb53e2695dfac8a
Tayfun Ateş <tayfun@pixerylabs.com>
2019-10-09 20:29:01 +0300

    Missing commit is completed.

--- Box2D/Common/b2Draw.h
+++ Box2D/Common/b2Draw.h
@@ -75,7 +75,7 @@ public:
 	virtual void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) = 0;
     
     /// Draw a solid closed polygon provided in CCW order by applying texture.
-    virtual void DrawTexturedSolidPolygon(const b2Vec2* vertices, const b2Vec2* textureCoordinates, uint32 texId, int32 vertexCount, const b2Color& color) = 0;
+    virtual void DrawTexturedSolidPolygon(const b2Vec2* vertices, const b2Vec2* textureCoordinates, uint32 glTexId, int matTexId, int32 vertexCount, const b2Color& color) = 0;
 
 	/// Draw a circle.
 	virtual void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color) = 0;
