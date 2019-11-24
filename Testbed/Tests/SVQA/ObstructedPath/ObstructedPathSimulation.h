e89624693971a359a384641aa26f42f654cc1d39
Tayfun Ateş <tayfun@pixerylabs.com>
2019-11-24 16:18:12 +0300

    Different types of dynamic objects can be added.

--- Testbed/Tests/SVQA/ObstructedPath/ObstructedPathSimulation.h
+++ Testbed/Tests/SVQA/ObstructedPath/ObstructedPathSimulation.h
@@ -28,22 +28,14 @@ namespace svqa {
             
             m_vInitialDropVelocity = b2Vec2(0.0f, -20.0f);
             
-            m_vThrowMin = b2Vec2(-10.0f, 40.0f);
-            m_vThrowMax = b2Vec2(-5.0f, 40.0f);
+            m_vThrowMin = b2Vec2(-30.0f, 50.0f);
+            m_vThrowMax = b2Vec2(30.0f, 50.0f);
             
             m_vObstacleMin = b2Vec2(-30.0f, 10.0f);
             m_vObstacleMax = b2Vec2(30.0f, 40.0f);
             
             m_bObstaclesCreated = false;
-            
-            {
-                b2BodyDef bd;
-                b2Body* ground = m_world->CreateBody(&bd);
-
-                b2EdgeShape shape;
-                shape.Set(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f));
-                ground->CreateFixture(&shape, 0.0f);
-            }
+            createBoundaries();
             
             SET_FILE_OUTPUT_TRUE(m_pSettings->outputFilePath)
         }
@@ -51,9 +43,26 @@ namespace svqa {
         virtual void Step(SettingsBase* settings) override
         {
             if(!m_bObstaclesCreated) {
+                //Obstacles are created in step since it requires GL context to be initialized
                 createObstacles();
             }
+            
+            const bool stable = isSceneStable();
+            const bool addObject = stable && m_nNumberOfObjects>0;
+            const bool terminateSimulation = m_nNumberOfObjects<=0 && stable;
+            
+            if(addObject) {
+                addSimulationObject();
+                m_nNumberOfObjects--;
+            }
+            
             Simulation::Step(settings);
+            
+            if(terminateSimulation)
+             {
+                 state.saveToJSONFile(m_world, "scene.json");
+                 FINISH_SIMULATION
+             }
         }
         
         virtual SimulationID getIdentifier() override
@@ -82,7 +91,7 @@ namespace svqa {
             float posY = RandomFloat(m_vObstacleMin.y, m_vObstacleMax.y);
             float orientation = RandomFloat(-M_PI / 4, M_PI / 4);
             
-            SimulationMaterial mat = SimulationMaterial(SimulationMaterial::RUBBER);
+            SimulationMaterial mat = SimulationMaterial(SimulationMaterial::METAL);
             
             b2BodyDef bd;
             bd.type = b2_dynamicBody;
@@ -111,8 +120,27 @@ namespace svqa {
                 SimulationColor col = SimulationColor((SimulationColor::TYPE) colorIndex);
                 staticObstacle->setTexture(mat.getTexture());
                 staticObstacle->setColor(col.GetColor(mat.type));
+                
+                state.add(ObjectState(staticObstacle, mat.type, col.type, object.type));
             }
             return true;
+        }
+        
+        void createBoundaries()
+        {
+            std::vector<std::pair<b2Vec2, b2Vec2>> boundaries;
+            boundaries.push_back(std::make_pair(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f)));
+            boundaries.push_back(std::make_pair(b2Vec2(-30.0f, 0.0f), b2Vec2(-30.0f, 50.0f)));
+            boundaries.push_back(std::make_pair(b2Vec2(30.0f, 0.0f), b2Vec2(30.0f, 50.0f)));
+            
+            for(auto&& bound: boundaries) {
+                b2BodyDef bd;
+                b2Body* boundBody = m_world->CreateBody(&bd);
+
+                b2EdgeShape shape;
+                shape.Set(bound.first, bound.second);
+                boundBody->CreateFixture(&shape, 0.0f);
+            }
         }
         
         bool m_bObstaclesCreated;
@@ -123,9 +151,37 @@ namespace svqa {
         b2Vec2 m_vObstacleMin;
         b2Vec2 m_vObstacleMax;
         b2Vec2 m_vInitialDropVelocity;
+        
+        const std::vector<SimulationObject::TYPE> m_vSimulationObjectTypes = {SimulationObject::SMALL_CUBE, SimulationObject::BIG_CUBE, SimulationObject::SMALL_HEXAGON, SimulationObject::BIG_HEXAGON,
+            SimulationObject::SMALL_TRIANGLE, SimulationObject::BIG_TRIANGLE };
+        
+        SceneState state;
 
         void addSimulationObject()
         {
+            float posX = RandomFloat(m_vThrowMin.x, m_vThrowMax.x);
+            float posY = RandomFloat(m_vThrowMin.y, m_vThrowMax.y);
+            
+            int objectIndex = randWithBound(m_vSimulationObjectTypes.size());
+            SimulationObject object = SimulationObject(m_vSimulationObjectTypes[objectIndex]);
+        
+            b2VisPolygonShape shape = object.getShape();
+            
+            SimulationMaterial mat = SimulationMaterial(SimulationMaterial::RUBBER);
+            
+            b2BodyDef bd;
+            bd.type = b2_dynamicBody;
+            bd.position = b2Vec2(posX, posY);
+            bd.linearVelocity = m_vInitialDropVelocity;
+            BODY* body = (BODY*) m_world->CreateBody(&bd);
+            body->CreateFixture(&shape, mat.getDensity());
+            
+            int colorIndex = randWithBound(m_nDistinctColorUsed);
+            SimulationColor col = SimulationColor((SimulationColor::TYPE) colorIndex);
+            body->setTexture(mat.getTexture());
+            body->setColor(col.GetColor(mat.type));
+            
+            state.add(ObjectState(body, mat.type, col.type, object.type));
         }
     };
 }
--- Testbed/Tests/SVQA/SimulationObject.h
+++ Testbed/Tests/SVQA/SimulationObject.h
@@ -18,7 +18,11 @@ public:
     {
         SMALL_CUBE = 0,
         BIG_CUBE = 1,
-        STANDARD_RECTANGLE = 2
+        STANDARD_RECTANGLE = 2,
+        SMALL_TRIANGLE = 3,
+        BIG_TRIANGLE = 4,
+        SMALL_HEXAGON = 5,
+        BIG_HEXAGON = 6
     };
 
     SimulationObject(TYPE t)
@@ -30,11 +34,19 @@ public:
     {
         switch (type) {
             case SMALL_CUBE:
-                return getCube(2.0f);
+                return getPolygon(2.0f, 4);
             case BIG_CUBE:
-                return getCube(4.0f);
+                return getPolygon(3.0f, 4);
             case STANDARD_RECTANGLE:
                 return getRectangle(8.0f, 2.0f);
+            case SMALL_HEXAGON:
+                return getPolygon(2.0f, 6);
+            case BIG_HEXAGON:
+                return getPolygon(3.0f, 6);
+            case SMALL_TRIANGLE:
+                return getPolygon(2.0f, 3);
+            case BIG_TRIANGLE:
+                return getPolygon(3.0f, 3);
             
             default:
                 break;
@@ -56,6 +68,36 @@ private:
     {
         b2VisPolygonShape shape;
         shape.SetAsBox(w, h);
+        return shape;
+    }
+    
+    b2Vec2 rotate(const b2Vec2& v, const float anglesInRadians)
+    {
+        const float s = sin(anglesInRadians);
+        const float c = cos(anglesInRadians);
+        b2Vec2 ret = v;
+        b2Vec2 translatedPoint = ret;
+        translatedPoint.x = ret.x * c - ret.y * s;
+        translatedPoint.y = ret.x * s + ret.y * c;
+        ret.x = translatedPoint.x;
+        ret.y = translatedPoint.y;
+        return ret;
+    }
+    
+    b2VisPolygonShape getPolygon(const float& edgeLength, const int& nPoints)
+    {
+        const float twoPi =  2.0f * 3.14159265f;
+        std::vector<b2Vec2> points(nPoints);
+        points[0] = b2Vec2(0.0f, -edgeLength);
+        b2Vec2 currentPoint = points[0];
+        const float rotationAngle = twoPi / nPoints;
+        for(size_t i = 1; i < points.size(); i++)
+        {
+            currentPoint = rotate(currentPoint, rotationAngle);
+            points[i] = currentPoint;
+        }
+        b2VisPolygonShape shape;
+        shape.Set(points.data(), nPoints);
         return shape;
     }
     
