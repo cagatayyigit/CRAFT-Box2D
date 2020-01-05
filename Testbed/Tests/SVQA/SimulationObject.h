1d764c17d2e2fe405f5fc3375b9965cd399f6eb2
cagatayyigit <cagatayyigit3@gmail.com>
2020-01-06 02:25:40 +0300

    Add skeleton of Scene1 Co-authored by: M. Samil Atesoglu <m.samilatesoglu@gmail.com>

--- Testbed/Tests/SVQA/SimulationObject.h
+++ Testbed/Tests/SVQA/SimulationObject.h
@@ -16,119 +16,138 @@ typedef std::shared_ptr<b2Shape> ShapePtr;
 class SimulationObject
 {
 public:
-    
-    enum TYPE
-    {
-        SMALL_CUBE = 0,
-        BIG_CUBE = 1,
-        STANDARD_RECTANGLE = 2,
-        SMALL_TRIANGLE = 3,
-        BIG_TRIANGLE = 4,
-        SMALL_HEXAGON = 5,
-        BIG_HEXAGON = 6,
-        WALL_PIN = 7,
-        ROPE_UNIT = 8,
-        SMALL_CIRCLE = 9,
-        BIG_CIRCLE = 10
-    };
 
-    SimulationObject(TYPE t)
-    {
-        type = t;
-    }
+	enum TYPE
+	{
+		SMALL_CUBE = 0,
+		BIG_CUBE = 1,
+		STANDARD_RECTANGLE = 2,
+		SMALL_TRIANGLE = 3,
+		BIG_TRIANGLE = 4,
+		SMALL_HEXAGON = 5,
+		BIG_HEXAGON = 6,
+		WALL_PIN = 7,
+		ROPE_UNIT = 8,
+		SMALL_CIRCLE = 9,
+		BIG_CIRCLE = 10,
+		BIG_RAMP = 11,
+		CUSTOM_RECTANGLE = 12
+	};
 
-    ShapePtr getShape()
-    {
-        switch (type) {
-            case SMALL_CUBE:
-                return std::make_shared<b2PolygonShape>(getPolygon(2.0f, 4));
-            case BIG_CUBE:
-                return std::make_shared<b2PolygonShape>(getPolygon(3.0f, 4));
-            case STANDARD_RECTANGLE:
-                return std::make_shared<b2PolygonShape>(getRectangle(8.0f, 2.0f));
-            case SMALL_HEXAGON:
-                return std::make_shared<b2PolygonShape>(getPolygon(2.0f, 6));
-            case BIG_HEXAGON:
-                return std::make_shared<b2PolygonShape>(getPolygon(3.0f, 6));
-            case SMALL_TRIANGLE:
-                return std::make_shared<b2PolygonShape>(getPolygon(2.0f, 3));
-            case BIG_TRIANGLE:
-                return std::make_shared<b2PolygonShape>(getPolygon(3.0f, 3));
-            case WALL_PIN:
-                return std::make_shared<b2PolygonShape>(getPolygon(0.5f, 4));
-            case ROPE_UNIT:
-                return std::make_shared<b2PolygonShape>(getPolygon(0.1f, 4));
-            case SMALL_CIRCLE:
-                return std::make_shared<b2CircleShape>(getCircle(2.0f));
-            case BIG_CIRCLE:
-                return std::make_shared<b2CircleShape>(getCircle(3.0f));
-            
-            default:
-                break;
-        }
-        return nullptr;
-    }
+	SimulationObject(TYPE t)
+	{
+		type = t;
+	}
 
-    TYPE type;
-    
-private:
-    
-    b2VisPolygonShape getCube(const float& edgeLength)
-    {
-        b2VisPolygonShape shape;
-        shape.SetAsBox(edgeLength, edgeLength);
-        return shape;
-    }
-    
-    b2VisPolygonShape getRectangle(const float& w, const float& h)
-    {
-        b2VisPolygonShape shape;
-        shape.SetAsBox(w, h);
-        return shape;
-    }
-    
-    b2Vec2 rotate(const b2Vec2& v, const float anglesInRadians)
-    {
-        const float s = sin(anglesInRadians);
-        const float c = cos(anglesInRadians);
-        b2Vec2 ret = v;
-        b2Vec2 translatedPoint = ret;
-        translatedPoint.x = ret.x * c - ret.y * s;
-        translatedPoint.y = ret.x * s + ret.y * c;
-        ret.x = translatedPoint.x;
-        ret.y = translatedPoint.y;
-        return ret;
-    }
-    
-    b2VisPolygonShape getPolygon(const float& edgeLength, const int& nPoints)
-    {
-        const float twoPi =  2.0f * 3.14159265f;
-        std::vector<b2Vec2> points(nPoints);
-        points[0] = b2Vec2(0.0f, -edgeLength);
-        b2Vec2 currentPoint = points[0];
-        const float rotationAngle = twoPi / nPoints;
-        for(size_t i = 1; i < points.size(); i++)
-        {
-            currentPoint = rotate(currentPoint, rotationAngle);
-            points[i] = currentPoint;
-        }
-        b2VisPolygonShape shape;
-        shape.Set(points.data(), nPoints);
-        return shape;
-    }
-    
-    b2CircleShape getCircle(const float& radius)
-    {
-        b2CircleShape circle;
-        circle.m_radius = radius;
-        return circle;
-    }
-    
+	ShapePtr getShape()
+	{
+		switch (type) {
+		case SMALL_CUBE:
+			return std::make_shared<b2PolygonShape>(getPolygon(2.0f, 4));
+		case BIG_CUBE:
+			return std::make_shared<b2PolygonShape>(getPolygon(3.0f, 4));
+		case STANDARD_RECTANGLE:
+			return std::make_shared<b2PolygonShape>(getRectangle(8.0f, 2.0f));
+		case SMALL_HEXAGON:
+			return std::make_shared<b2PolygonShape>(getPolygon(2.0f, 6));
+		case BIG_HEXAGON:
+			return std::make_shared<b2PolygonShape>(getPolygon(3.0f, 6));
+		case SMALL_TRIANGLE:
+			return std::make_shared<b2PolygonShape>(getPolygon(2.0f, 3));
+		case BIG_TRIANGLE:
+			return std::make_shared<b2PolygonShape>(getPolygon(3.0f, 3));
+		case WALL_PIN:
+			return std::make_shared<b2PolygonShape>(getPolygon(0.5f, 4));
+		case ROPE_UNIT:
+			return std::make_shared<b2PolygonShape>(getPolygon(0.1f, 4));
+		case SMALL_CIRCLE:
+			return std::make_shared<b2CircleShape>(getCircle(2.0f));
+		case BIG_CIRCLE:
+			return std::make_shared<b2CircleShape>(getCircle(3.0f));
+		case BIG_RAMP:
+			return std::make_shared<b2PolygonShape>(getPolygon(3.0f, 3));
+		default:
+			break;
+		}
+		return nullptr;
+	}
+
+	TYPE type;
+
+
+
+	static b2VisPolygonShape getCube(const float& edgeLength)
+	{
+		b2VisPolygonShape shape;
+		shape.SetAsBox(edgeLength, edgeLength);
+		return shape;
+	}
+
+	static b2VisPolygonShape getRectangle(const float& w, const float& h)
+	{
+		b2VisPolygonShape shape;
+		shape.SetAsBox(w, h);
+		return shape;
+	}
+
+	static b2VisPolygonShape getRectangle(const float& w, const float& h,VECTOR center,float32 angle)
+	{
+		b2VisPolygonShape shape;
+		shape.SetAsBox(w, h,center,angle);
+		return shape;
+	}
+
+	static b2Vec2 rotate(const b2Vec2& v, const float anglesInRadians)
+	{
+		const float s = sin(anglesInRadians);
+		const float c = cos(anglesInRadians);
+		b2Vec2 ret = v;
+		b2Vec2 translatedPoint = ret;
+		translatedPoint.x = ret.x * c - ret.y * s;
+		translatedPoint.y = ret.x * s + ret.y * c;
+		ret.x = translatedPoint.x;
+		ret.y = translatedPoint.y;
+		return ret;
+	}
+
+	static b2VisPolygonShape getPolygon(const float& edgeLength, const int& nPoints)
+	{
+		const float twoPi = 2.0f * 3.14159265f;
+		std::vector<b2Vec2> points(nPoints);
+		points[0] = b2Vec2(0.0f, -edgeLength);
+		b2Vec2 currentPoint = points[0];
+		const float rotationAngle = twoPi / nPoints;
+		for (size_t i = 1; i < points.size(); i++)
+		{
+			currentPoint = rotate(currentPoint, rotationAngle);
+			points[i] = currentPoint;
+		}
+		b2VisPolygonShape shape;
+		shape.Set(points.data(), nPoints);
+		return shape;
+	}
+
+	static b2CircleShape getCircle(const float& radius)
+	{
+		b2CircleShape circle;
+		circle.m_radius = radius;
+		return circle;
+	}
+
+	static b2VisPolygonShape getRightTriangle(const float& hEdgeLength, const float& vEdgeLength)
+	{
+		b2VisPolygonShape shape;
+		// TODO
+	}
+
+
+
 };
 
-NLOHMANN_JSON_SERIALIZE_ENUM( SimulationObject::TYPE, {
-    {SimulationObject::SMALL_CUBE, "small_cube"},
-    {SimulationObject::BIG_CUBE, "big_cube"}
-})
+NLOHMANN_JSON_SERIALIZE_ENUM(SimulationObject::TYPE, {
+	{SimulationObject::SMALL_CUBE, "small_cube"},
+	{SimulationObject::BIG_CUBE, "big_cube"},
+	{SimulationObject::CUSTOM_RECTANGLE, "custom_rectangle"}
+	})
 
 #endif /* SimulationObject_h */
