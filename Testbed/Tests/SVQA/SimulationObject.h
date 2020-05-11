14d1d34a8e8a5083f61800858ceb739ced5b8040
Çağatay <cagatayyigit3@gmail.com>
2020-05-12 01:55:58 +0300

    Resize all of the objects in Simulation 1

--- Testbed/Tests/SVQA/SimulationObject.h
+++ Testbed/Tests/SVQA/SimulationObject.h
@@ -335,7 +335,7 @@ public:
     static b2ChainShape* getBasketShape()
     {
         b2ChainShape* shape = new b2ChainShape;
-        std::vector<b2Vec2> vertices = {b2Vec2(-5, 5), b2Vec2(-5, -5), b2Vec2(5, -5), b2Vec2(5, 5)};
+        std::vector<b2Vec2> vertices = {b2Vec2(-3.5f, 3.5), b2Vec2(-3.5, -3.5), b2Vec2(3.5, -3.5), b2Vec2(3.5, 3.5)};
         shape->CreateChain(vertices.data(), vertices.size());
         return shape;
     }
