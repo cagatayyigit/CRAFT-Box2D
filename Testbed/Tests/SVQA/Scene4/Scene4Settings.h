b29d1bb10f6f57dbca33ee73ebae79148db2e630
Çağatay Yiğit <cagatayyigit3@gmail.com>
2020-05-31 17:00:55 +0300

    Create scene4

---
+++ Testbed/Tests/SVQA/Scene4/Scene4Settings.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Settings.h"
+
+namespace svqa
+{
+	struct Scene4Settings : Settings
+	{
+		typedef std::shared_ptr<Scene4Settings> Ptr;
+
+		Scene4Settings() {}
+
+		int numberOfObjects;
+		int numberOfObstacles;
+
+		void to_json(json& j) {
+			j.emplace("numberOfObjects", this->numberOfObjects);
+			j.emplace("numberOfObstacles", this->numberOfObstacles);
+			Settings::to_json(j);
+		}
+
+		void from_json(const json& j) {
+			Settings::from_json(j);
+			j.at("numberOfObjects").get_to(this->numberOfObjects);
+			j.at("numberOfObstacles").get_to(this->numberOfObstacles);
+		}
+	};
+}
---
+++ Testbed/Tests/SVQA/Scene4/Scene4Simulation.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include "Scene4Settings.h"
+#include "SimulationBase.h"
+#include "Box2D/Extension/b2VisBody.hpp"
+#include "Box2D/Extension/b2VisPolygonShape.hpp"
+#include <math.h> 
+
+namespace svqa {
+	class Scene4Simulation : public SimulationBase
+	{
+	public:
+		typedef std::shared_ptr<Scene4Simulation> Ptr;
+		Scene4Simulation(Scene4Settings::Ptr settings) : SimulationBase(settings)
+		{
+			m_nNumberOfObjects = settings->numberOfObjects;
+			m_nNumberOfObstacles = settings->numberOfObstacles;
+
+			SET_FILE_OUTPUT_TRUE(m_pSettings->outputVideoPath)
+		}
+
+		virtual SimulationID getIdentifier() override
+		{
+			return SimulationID::ID_Scene4;
+		}
+
+		void InitializeScene() override {
+
+			std::vector<SimulationObject::Color> distinc_colors;
+			while(distinc_colors.size() < 5) {
+				SimulationObject::Color c = SimulationObject::getRandomColor();
+				if (std::find(distinc_colors.begin(), distinc_colors.end(), c) == distinc_colors.end()) {
+					distinc_colors.push_back(c);
+				}
+			}
+			
+			// Basket
+			AddTargetBasket(b2Vec2(21.0f, -1.2f), 0.0f);
+
+
+
+			AddDynamicObject(
+				b2Vec2(RandomFloatFromHardware(-17.0f, -15.0f), 24.5f),
+				b2Vec2(RandomFloatFromHardware(5.0f, 15.0f), 0.0f),
+				SimulationObject::Shape::CIRCLE,
+				distinc_colors[0],
+				SimulationObject::getRandomSize());
+			AddStaticObject(b2Vec2(-18.0f, 23.0f), 0, SimulationObject::STATIC_PLATFORM);
+			AddStaticObject(b2Vec2(-8.0f, 23.0f), 0, SimulationObject::STATIC_PLATFORM);
+			AddStaticObject(b2Vec2(-2.0f, 23.0f), 0, SimulationObject::STATIC_PLATFORM);
+
+
+
+			AddDynamicObject(
+				b2Vec2(RandomFloatFromHardware(-18.0f, -16.0f), 18.5f),
+				b2Vec2(RandomFloatFromHardware(5.0f, 15.0f), 0.0f),
+				SimulationObject::Shape::CIRCLE,
+				distinc_colors[1],
+				SimulationObject::getRandomSize());
+			AddStaticObject(b2Vec2(-18.0f, 16.0f), 0, SimulationObject::STATIC_PLATFORM);
+			AddStaticObject(b2Vec2(-9.0f, 16.0f), 0, SimulationObject::STATIC_PLATFORM);
+
+
+
+
+
+			AddDynamicObject(
+				b2Vec2(RandomFloatFromHardware(-18.0f, -17.0f), 11.5f),
+				b2Vec2(RandomFloatFromHardware(5.0f, 15.0f), 0.0f),
+				SimulationObject::Shape::CIRCLE,
+				distinc_colors[2],
+				SimulationObject::getRandomSize());
+			AddStaticObject(b2Vec2(-18.0f, 10.0f), 0, SimulationObject::STATIC_PLATFORM);
+
+
+
+
+
+
+			AddDynamicObject(
+				b2Vec2(-18.0f, 5.5f),
+				b2Vec2(RandomFloatFromHardware(5.0f, 15.0f), 0.0f),
+				SimulationObject::Shape::CIRCLE,
+				distinc_colors[3],
+				SimulationObject::getRandomSize());
+			AddStaticObject(b2Vec2(-18.0f, 4.0f), 0, SimulationObject::STATIC_PLATFORM);
+			AddStaticObject(b2Vec2(-4.0f, 4.0f), 0, SimulationObject::STATIC_PLATFORM);
+			AddStaticObject(b2Vec2(9.5f, 4.0f), 0, SimulationObject::STATIC_PLATFORM);
+			
+
+			
+			
+
+
+
+		}
+
+	private:
+
+		bool m_bObstaclesCreated;
+		int m_nNumberOfObjects;
+		int m_nNumberOfObstacles;
+		float32 m_fSpeed;
+		b2Vec2 m_vMovingObjPosition;
+		b2Vec2 m_vStagnantObjPosition;
+		b2Vec2 m_vObstaclePosition;
+		b2Vec2 m_vInitialDropVelocity;
+	};
+}
