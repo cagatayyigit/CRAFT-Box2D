f71d87013cda2e2fba82a5c226a7e92db7ab373c
cagatayyigit <cagatayyigit3@gmail.com>
2020-07-15 19:37:58 +0300

    Add Scene 10 (Mert - Dynamic Basket), without dynamic basket

--- Testbed/Tests/SVQA/Scenes/Scene10Simulation.h
+++ Testbed/Tests/SVQA/Scenes/Scene10Simulation.h
@@ -25,7 +25,46 @@ namespace svqa {
         }
 
         void InitializeScene() override {
+            
+            // Basket
+            AddTargetBasket(b2Vec2(RandomFloatFromHardware(3.0, 5.0), -1.2f), 0.0f);
+            
+            // top
+            float height = RandomFloatFromHardware(26, 30);
+            AddStaticObject(b2Vec2(-8.0f, height),44 * M_PI / RandomFloatFromHardware(51.2, 57.0),  SimulationObject::STATIC_PLATFORM);
+            AddRandomDynamicObject(
+                b2Vec2(-8.0f, height + 4.0f),
+                b2Vec2(0.0f, 0.0f)
+            );
+            
+            
+            // Middle Right Floor dynamic basket
+            AddStaticObject(b2Vec2(11.0f, 17.0f), 0, SimulationObject::STATIC_PLATFORM);
+            
+            
+            // Middle Inclined Left  Floor
+            AddStaticObject(b2Vec2(-17.0f, 17.0f), 43 * M_PI / RandomFloatFromHardware(51.2, 57.0), SimulationObject::STATIC_PLATFORM);
+            AddRandomDynamicObject(
+                    b2Vec2(-19.0f,  22.0f),
+                    b2Vec2(0.0f, 0.0f)
+                );
+            
+            
+            // Bottom Inclıned Right Floor
+            float x = RandomFloatFromHardware(14, 18);
+            AddStaticObject(b2Vec2(x, 10.0f),- 46 * M_PI / RandomFloatFromHardware(51.2, 57.0),SimulationObject::STATIC_PLATFORM);
+            AddRandomDynamicObject(
+                b2Vec2(16.0f, 14.0f),
+                b2Vec2(0.0f, 0.0f)
+            );
 
+            
+            
+            // Bottom Left  Floor
+            AddStaticObject(b2Vec2(-8.0f, 8.0f), 0, SimulationObject::STATIC_PLATFORM);
+            
+
+            
         }
 
     private:
