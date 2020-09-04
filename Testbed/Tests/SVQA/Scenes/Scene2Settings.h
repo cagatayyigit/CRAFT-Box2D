19276379cca5a5bf90310b5fc5e36c7988163dda
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2020-09-04 23:20:45 +0300

    Remove unnecessary controller parameters

--- Testbed/Tests/SVQA/Scenes/Scene2Settings.h
+++ Testbed/Tests/SVQA/Scenes/Scene2Settings.h
@@ -15,15 +15,11 @@ namespace svqa
 		int numberOfObstacles;
 
 		void to_json(json& j) {
-			j.emplace("numberOfObjects", this->numberOfObjects);
-			j.emplace("numberOfObstacles", this->numberOfObstacles);
 			Settings::to_json(j);
 		}
 
 		void from_json(const json& j) {
 			Settings::from_json(j);
-			j.at("numberOfObjects").get_to(this->numberOfObjects);
-			j.at("numberOfObstacles").get_to(this->numberOfObstacles);
 		}
 	};
 }
