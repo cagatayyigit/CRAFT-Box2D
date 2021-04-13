77c47fe4a46591dbd357f6f208545a69593a5785
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-13 09:24:18 +0300

    Add constructor for textureless materials

--- Testbed/Framework/SimulationMaterial.cpp
+++ Testbed/Framework/SimulationMaterial.cpp
@@ -7,46 +7,30 @@
 
 #include "SimulationMaterial.h"
 
-// const std::string SimulationMaterial::metalFilePath = "Data/Textures/metal.png";
-// const std::string SimulationMaterial::rubberFilePath = "Data/Textures/rubber.png";
 const std::string SimulationMaterial::eyesFilePath = "Data/Textures/eyes.png";
-const std::string SimulationMaterial::platformFilePath = "Data/Textures/platform.png";
-const std::string SimulationMaterial::sensorFilePath = "Data/Textures/sensor.png";
-// b2VisTexture::Ptr SimulationMaterial::metalTexture;
-// b2VisTexture::Ptr SimulationMaterial::rubberTexture;
+
 b2VisTexture::Ptr SimulationMaterial::eyesTexture;
 b2VisTexture::Ptr SimulationMaterial::platformTexture;
 b2VisTexture::Ptr SimulationMaterial::sensorTexture;
 
 b2VisTexture::Ptr SimulationMaterial::getTexture()
 {
-    //if (!SimulationMaterial::metalTexture) {
-    //    SimulationMaterial::metalTexture = b2VisTexture::Ptr(new b2VisTexture(SimulationMaterial::metalFilePath, SimulationMaterial::METAL));
-    //}
-    //
-    //if (!SimulationMaterial::rubberTexture) {
-    //    SimulationMaterial::rubberTexture = b2VisTexture::Ptr(new b2VisTexture(SimulationMaterial::rubberFilePath, SimulationMaterial::RUBBER));
-    //}
     if (!SimulationMaterial::platformTexture) {
-        SimulationMaterial::platformTexture = b2VisTexture::Ptr(new b2VisTexture(SimulationMaterial::platformFilePath, SimulationMaterial::TYPE::PLATFORM));
+        SimulationMaterial::platformTexture = b2VisTexture::Ptr(new b2VisTexture( SimulationMaterial::TYPE::PLATFORM));
     }
-
+    
     if (!SimulationMaterial::sensorTexture) {
-        SimulationMaterial::sensorTexture = b2VisTexture::Ptr(new b2VisTexture(SimulationMaterial::sensorFilePath, SimulationMaterial::TYPE::SENSOR));
+        SimulationMaterial::sensorTexture = b2VisTexture::Ptr(new b2VisTexture( SimulationMaterial::TYPE::SENSOR));
     }
-
+    
     if (!SimulationMaterial::eyesTexture) {
         SimulationMaterial::eyesTexture = b2VisTexture::Ptr(new b2VisTexture(SimulationMaterial::eyesFilePath, SimulationMaterial::TYPE::EYES));
     }
-
-    //if (type == METAL) {
-    //    return SimulationMaterial::metalTexture;
-    //}
-
+    
     if (type == EYES) {
         return SimulationMaterial::eyesTexture;
     }
-
+     
     if (type == PLATFORM) {
         return SimulationMaterial::platformTexture;
     }
@@ -54,6 +38,6 @@ b2VisTexture::Ptr SimulationMaterial::getTexture()
     if (type == SENSOR) {
         return SimulationMaterial::sensorTexture;
     }
-
+    
     return nullptr;
 }
