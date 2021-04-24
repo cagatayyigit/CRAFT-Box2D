07adff32bf60fbb51972dbfe04b760a954fa105c
cagatayyigit <cagatayyigit3@gmail.com>
2021-04-24 16:58:04 +0300

    Add new scene 11
    need polishing

--- Testbed/Tests/SVQA/Scenes/Scene11Simulation.h
+++ Testbed/Tests/SVQA/Scenes/Scene11Simulation.h
@@ -22,66 +22,62 @@ namespace svqa {
         }
         
         void InitializeScene() override {
-            
-            
+
             // Basket
-            AddTargetBasket(b2Vec2(RandomFloatFromHardware(-3.0, 3.0), -1.2f), 0.0f);
+            float basket_pos_x = getExtremeCases(m_sStaticObjectOrientationType, 18.0f, 21.0f);
+            AddTargetBasket(b2Vec2(basket_pos_x, -1.2f), 0.0f);
             
-            
-            // Bottom Left  Floor
-            AddStaticObject(b2Vec2(-12.0f, 6.0f), 0, SimulationObject::STATIC_PLATFORM);
-            
-            // Bottom Right Floor
-            float x = RandomFloatFromHardware(12, 18);
-            AddStaticObject(b2Vec2(x, 6.0f), 0, SimulationObject::STATIC_PLATFORM);
-            
-            
-            // Bottom Left  Vertical Floor
-            AddStaticObject(b2Vec2(-28.0f, 12.0f), 4 * M_PI / 5, SimulationObject::STATIC_PLATFORM);
-            AddRandomDynamicObject(
-                b2Vec2(-23.0f,14.0f),
-                b2Vec2(0.0f, 0.0f)
-            );
-            
-            
-            // Middle Left  Floor
-            AddStaticObject(b2Vec2(-5.0f, 18.0f), 50 * M_PI / RandomFloatFromHardware(51.2, 57.0), SimulationObject::STATIC_PLATFORM);
-            
-            AddRandomDynamicObject(
-                b2Vec2(-5.0f,  20.0f),
-                b2Vec2(0.0f, 0.0f)
-            );
-            
-            
-            // Middle Right Floor
-            AddStaticObject(b2Vec2(16.0f, RandomFloatFromHardware(22, 26)), - 50 * M_PI / RandomFloatFromHardware(51.2, 57.0), SimulationObject::STATIC_PLATFORM);
-            AddRandomDynamicObject(
-                b2Vec2(16.0f,  29.0f),
-                b2Vec2(0.0f, 0.0f)
-            );
-            
-            float height = RandomFloatFromHardware(26, 30);
-            
-            AddRandomDynamicObject(
-                b2Vec2(-2.0f, height + 9.0f),
-                b2Vec2(-8.0f, -8.0f)
-            );
-            
-            AddRandomDynamicObject(
-                b2Vec2(-7.0f, height + 2.0f),
-                b2Vec2(0.0f, 0.0f)
-            );
-            AddStaticObject(b2Vec2(-10.0f, height), 0, SimulationObject::STATIC_PLATFORM);
-            
-            
-            // Stationary Bottom-Left
-            AddRandomDynamicObject(
-                b2Vec2(-8.0f, 9.0f),
-                b2Vec2(0.0f, 0.0f)
-            );
-            
+            // Right platform
+            float right_platform_pos_x = 12.0f;
+            AddStaticObject(b2Vec2(right_platform_pos_x, 5.0f), 0, SimulationObject::STATIC_PLATFORM);
+
+            // Left platform
+            float left_platform_pos_x = -12.0f;
+            AddStaticObject(b2Vec2(left_platform_pos_x, 5.0f), 0, SimulationObject::STATIC_PLATFORM);
+
+            // Static ball
+            float static_ball_pos_y = 3.5f;
+            AddStaticObject(b2Vec2(0.0f, static_ball_pos_y), 0, SimulationObject::STATIC_BALL);
+
+
+            // Right inclined-platform     
+            AddStaticObject(b2Vec2(right_platform_pos_x, 32.0f), M_PI / 5.0f , SimulationObject::STATIC_PLATFORM);
+
+            // Left inclined-platform
+            AddStaticObject(b2Vec2(left_platform_pos_x + 4.0f, 18.0f), M_PI / 5.0f , SimulationObject::STATIC_PLATFORM);
+
+
+
+            if (m_bIncludeDynamicObjects) {
+
+                // Circle on the Right platform
+                AddRandomDynamicObject(
+                    b2Vec2(right_platform_pos_x, 6.5f),
+                    b2Vec2(-15.0f, 0.0f),
+                    0b000,
+                    SimulationObject::CIRCLE
+                );
+
+                // Circle on the Left platform
+                AddRandomDynamicObject(
+                    b2Vec2(left_platform_pos_x, 6.5f),
+                    b2Vec2(15.0f, 0.0f),
+                    0b000,
+                    SimulationObject::CIRCLE
+                );
+
+                // Circle on the Right inclined-platform
+                AddRandomDynamicObject(
+                    b2Vec2(right_platform_pos_x + 2.0f, 37.5f),
+                    b2Vec2(0.0f, 0.0f),
+                    0b000,
+                    SimulationObject::CIRCLE
+                );
+            }
         }
-        
+            
+
+
     private:
 
     };
