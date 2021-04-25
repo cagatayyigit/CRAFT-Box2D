168eaed9a398ceed8351a2fec05f94b90a453089
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-25 14:37:31 +0300

    Basic static implementation of scene 13

--- Testbed/Tests/SVQA/Scenes/Scene13Simulation.h
+++ Testbed/Tests/SVQA/Scenes/Scene13Simulation.h
@@ -49,19 +49,44 @@ namespace svqa {
             float angle = M_PI / 5.0f;
             // Right upper inclined platform part 1
             float right_upper_platform_pos_x = 18.0f;
-            float right_upper_platform_pos_y = 25.0f;
+            float right_upper_platform_pos_y = 28.0f;
             AddStaticObject(b2Vec2(right_upper_platform_pos_x, right_upper_platform_pos_y), angle, SimulationObject::STATIC_PLATFORM);
             // Right upper inclined platform part 2
             float right_upper_platform_2_pos_x = right_upper_platform_pos_x - cos(angle) * 8.0 * 2;
             float right_upper_platform_2_pos_y = right_upper_platform_pos_y - sin(angle) * 8.0 * 2;
             AddStaticObject(b2Vec2(right_upper_platform_2_pos_x, right_upper_platform_2_pos_y), angle, SimulationObject::STATIC_PLATFORM);
 
+            // Platforms that are composed of two or more parts can be implemented like this.
+            // Right lower inclined platform
+            angle = M_PI / 5.0f;
+            // Right lower inclined platform part 1
+            float right_lower_platform_pos_x = 18.0f;
+            float right_lower_platform_pos_y = 20.0f;
+            AddStaticObject(b2Vec2(right_lower_platform_pos_x, right_lower_platform_pos_y), angle, SimulationObject::STATIC_PLATFORM);
+            // Right lower inclined platform part 2
+            float right_lower_platform_2_pos_x = right_lower_platform_pos_x - cos(angle) * 8.0 * 2;
+            float right_lower_platform_2_pos_y = right_lower_platform_pos_y - sin(angle) * 8.0 * 2;
+            AddStaticObject(b2Vec2(right_lower_platform_2_pos_x, right_lower_platform_2_pos_y), angle, SimulationObject::STATIC_PLATFORM);
+
             // Upper platform
             float upper_platform_pos_x = -12.0f;
-            float upper_platform_pos_y = 32.0f;
+            float upper_platform_pos_y = 36.0f;
             AddStaticObject(b2Vec2(upper_platform_pos_x, upper_platform_pos_y), 0, SimulationObject::STATIC_PLATFORM);
             
-
+            if (m_bIncludeDynamicObjects) {
+                AddRandomDynamicObject(
+                    b2Vec2(b2Vec2(upper_platform_pos_x, upper_platform_pos_y + 1)),
+                    b2Vec2(0.0f, 0.0f)
+                );
+                AddRandomDynamicObject(
+                    b2Vec2(b2Vec2(right_lower_platform_pos_x, right_lower_platform_pos_y + 1)),
+                    b2Vec2(0.0f, 0.0f)
+                );
+                AddRandomDynamicObject(
+                    b2Vec2(b2Vec2(right_upper_platform_pos_x, right_upper_platform_pos_y + 1)),
+                    b2Vec2(0.0f, 0.0f)
+                );
+            }
         }
             
 
