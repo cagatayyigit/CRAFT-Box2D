f812eb24ca2c75503bb2aa843ba1d5effbbe9ce1
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-25 14:26:12 +0300

    Add some scene elements to scene 13

--- Testbed/Tests/SVQA/Scenes/Scene13Simulation.h
+++ Testbed/Tests/SVQA/Scenes/Scene13Simulation.h
@@ -24,7 +24,43 @@ namespace svqa {
         void InitializeScene() override {
 
 
+            // Basket   
+            float basket_pos_x = -18.0f;
+            AddTargetBasket(b2Vec2(basket_pos_x, -1.2f), 0.0f);
 
+
+            // Static ball right
+            float static_ball_1_pos_x = -6.0f;
+            float static_ball_1_pos_y = 10.0f;
+            AddStaticObject(b2Vec2(static_ball_1_pos_x, static_ball_1_pos_y), 0, SimulationObject::STATIC_BALL);
+
+            // Static ball middle
+            float static_ball_2_pos_x = -12.0f;
+            float static_ball_2_pos_y = 20.0f;
+            AddStaticObject(b2Vec2(static_ball_2_pos_x, static_ball_2_pos_y), 0, SimulationObject::STATIC_BALL);
+
+            // Static ball left
+            float static_ball_3_pos_x = -18.0f;
+            float static_ball_3_pos_y = 30.0f;
+            AddStaticObject(b2Vec2(static_ball_3_pos_x, static_ball_3_pos_y), 0, SimulationObject::STATIC_BALL);
+
+            // Platforms that are composed of two or more parts can be implemented like this.
+            // Right upper inclined platform
+            float angle = M_PI / 5.0f;
+            // Right upper inclined platform part 1
+            float right_upper_platform_pos_x = 18.0f;
+            float right_upper_platform_pos_y = 25.0f;
+            AddStaticObject(b2Vec2(right_upper_platform_pos_x, right_upper_platform_pos_y), angle, SimulationObject::STATIC_PLATFORM);
+            // Right upper inclined platform part 2
+            float right_upper_platform_2_pos_x = right_upper_platform_pos_x - cos(angle) * 8.0 * 2;
+            float right_upper_platform_2_pos_y = right_upper_platform_pos_y - sin(angle) * 8.0 * 2;
+            AddStaticObject(b2Vec2(right_upper_platform_2_pos_x, right_upper_platform_2_pos_y), angle, SimulationObject::STATIC_PLATFORM);
+
+            // Upper platform
+            float upper_platform_pos_x = -12.0f;
+            float upper_platform_pos_y = 32.0f;
+            AddStaticObject(b2Vec2(upper_platform_pos_x, upper_platform_pos_y), 0, SimulationObject::STATIC_PLATFORM);
+            
 
         }
             
