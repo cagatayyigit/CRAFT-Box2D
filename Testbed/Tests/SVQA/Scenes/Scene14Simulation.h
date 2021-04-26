9c33f9d2db37fbf33645625e5668ebd9356f0282
cagatayyigit <cagatayyigit3@gmail.com>
2021-04-26 22:08:39 +0300

    Fine-tuning scene 14

--- Testbed/Tests/SVQA/Scenes/Scene14Simulation.h
+++ Testbed/Tests/SVQA/Scenes/Scene14Simulation.h
@@ -24,7 +24,7 @@ namespace svqa {
         void InitializeScene() override {
 
             // Basket
-            float basket_pos_x = getExtremeCases(m_sStaticObjectOrientationType, -1.0f, 1.0f);
+            float basket_pos_x = getExtremeCases(m_sStaticObjectOrientationType, -1.5f, 1.5f);
             AddTargetBasket(b2Vec2(basket_pos_x, -1.2f), 0.0f);
 
 
@@ -35,10 +35,17 @@ namespace svqa {
 
             // Right inclined-platform     
             float right_platform_pos_x = 12.0f;
-            AddStaticObject(b2Vec2(right_platform_pos_x - 5.0f, 30.0f), M_PI / 6.0f, SimulationObject::STATIC_PLATFORM);
+            AddStaticObject(
+                b2Vec2(right_platform_pos_x - 5.0f, 30.0f),
+                M_PI / getExtremeCases(m_sStaticObjectOrientationType, 4.0f, 7.0f),
+                SimulationObject::STATIC_PLATFORM
+            );
 
             // Left inclined-platform
-            AddStaticObject(b2Vec2(left_platform_pos_x - 5.0f, 28.0f), -M_PI / 5.0f, SimulationObject::STATIC_PLATFORM);
+            AddStaticObject(
+                b2Vec2(left_platform_pos_x - 5.0f, 28.0f), 
+                -M_PI / getExtremeCases(m_sStaticObjectOrientationType, 4.0f, 7.0f),
+                SimulationObject::STATIC_PLATFORM);
             
 
 
@@ -46,26 +53,22 @@ namespace svqa {
 
               
                 AddRandomDynamicObject(
-                    b2Vec2(left_platform_pos_x - 2.0f, 14.0f),
-                    b2Vec2(4.0f, 0.0f),
+                    b2Vec2(left_platform_pos_x - 4.0f, 14.0f),
+                    b2Vec2(getExtremeCases(m_sStaticObjectOrientationType, 3.0f, 7.0f), 0.0f),
                     0b000,
                     SimulationObject::CIRCLE
                 );
 
                
                 AddRandomDynamicObject(
-                    b2Vec2(right_platform_pos_x - 5.0f, 32.5f),
-                    b2Vec2(0.0f, 0.0f),
-                    0b000,
-                    SimulationObject::TRIANGLE
+                    b2Vec2(right_platform_pos_x - 5.0f, getExtremeCases(m_sStaticObjectOrientationType, 32.0f, 34.0f)),
+                    b2Vec2(0.0f, 0.0f)
                 );
 
                 
                 AddRandomDynamicObject(
-                    b2Vec2(left_platform_pos_x - 5.0f, 36.5f),
-                    b2Vec2(0.0f, 0.0f),
-                    0b000,
-                    SimulationObject::CUBE
+                    b2Vec2(left_platform_pos_x - 5.0f, getExtremeCases(m_sStaticObjectOrientationType, 36.0f, 38.0f)),
+                    b2Vec2(0.0f, 0.0f)
                 );
             }
 
